e57e460f3838285181de083d6d0bbfba662e7cdd
Chris Woodall <cmwoodall@game7.net>
2010-11-08 10:24:00 -0700

    correct form 'beginTag' to render mvc parameters (page, controller, action) as hidden for elements rather then 'action'

--- lib/form.php
+++ lib/form.php
@@ -17,22 +17,29 @@ class FormHelper
   function begin( $options = array(), $controller = null, $action = null )
   {
     
-    $htmlAttributes = array(
-      'method' => 'post'      
-    );
+    $htmlAttributes = $options;
+    
+    if (!$htmlAttributes['method'])
+      $htmlAttributes['method'] = 'POST';
     
     if ( $action && $controller ) {
       $url = $this->url->action( $controller, $action ); 
       $htmlAttributes = array_merge($htmlAttributes, array( 'action' => $url ));
     }    
-    return $this->html->beginTag( 'form', $htmlAttributes );  
+    
+    $output .= $this->html->beginTag( 'form', $htmlAttributes );
+    $output .= $this->html->hidden( 'page', $_GET['page']);
+    if ($controller) $output .= $this->html->hidden( 'controller', $controller);
+    if ($action) $output .= $this->html->hidden( 'action', $action);
+    
+    return $output;
     
   }
 
   function end() {
     return $this->html->endTag( 'form' );
   }
-  
+    
   function anti_forgery_token( $key ) {
     return wp_nonce_field( $key );
   }
